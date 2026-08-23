3218cd0e3b511fbb617aefc32902933964e09c5c
Sobhan <sobhan.ganji@gmail.com>
2026-08-22 21:51:51 -0400

    Delete lib directory

--- lib/lang-cookie.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import type { Lang } from "./i18n";
-
-export function getLang(): Lang {
-  const c = cookies().get("mrjudge_lang")?.value;
-  return c === "fa" ? "fa" : "en";
-}
